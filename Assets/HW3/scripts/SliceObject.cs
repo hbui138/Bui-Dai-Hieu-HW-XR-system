6b30ad18b8df442fe9c42f7adf061f61243eab92
Hieu Bui <hbui22@student.oulu.fi>
2024-03-05 17:41:06 +0200

    Add test sclicer

---
+++ Assets/HW3/scripts/SliceObject.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;
+
+public class SliceObject : MonoBehaviour
+{
+    public Transform startSlicePoint;
+    public Transform endSlicePoint;
+    public VelocityEstimator velocityEstimator;
+    public LayerMask sliceable;
+    public float cutForce = 2000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bool hasHit = Physics.Linecast(startSlicePoint.position, endSlicePoint.position, out RaycastHit hit, sliceable);
+        if (hasHit)
+        {
+            GameObject target = hit.transform.gameObject;
+            Slice(target);
+        }
+    }
+
+    public void Slice(GameObject target)
+    {
+        Vector3 velocity = velocityEstimator.GetVelocityEstimate();
+        Vector3 plane = Vector3.Cross(endSlicePoint.position - startSlicePoint.position, velocity);
+        plane.Normalize();
+        SlicedHull hull = target.Slice(endSlicePoint.position, plane);
+
+        if(hull != null)
+        {
+            GameObject upperHull = hull.CreateUpperHull();
+            SetUpSlicedComponent(upperHull);
+            GameObject lowerHull = hull.CreateLowerHull();
+            SetUpSlicedComponent(lowerHull);
+        }
+    }
+
+    public void SetUpSlicedComponent(GameObject slicedObject)
+    {
+        Rigidbody rb = slicedObject.AddComponent<Rigidbody>();
+        MeshCollider collider = slicedObject.AddComponent<MeshCollider>();
+        collider.convex = true;
+        rb.AddExplosionForce(cutForce, slicedObject.transform.position, 1);
+    }
+}
